b3466578963e17dbdf4521f35567a048497f8af1
Richard Huang <flyerhzm@gmail.com>
2024-07-16 20:05:09 +0800

    require syntax_tree_ext/parent_node_ext back

--- lib/syntax_tree_ext.rb
+++ lib/syntax_tree_ext.rb
@@ -4,6 +4,7 @@ require_relative "syntax_tree_ext/version"
 
 require 'syntax_tree'
 require_relative "syntax_tree_ext/source_ext"
+require_relative "syntax_tree_ext/parent_node_ext"
 
 module SyntaxTreeExt
   class Error < StandardError; end
